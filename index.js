60e126a70f2818b7f91518cc2d2c73acd2ef7fa8
0re5ama <shahbibek10@gmail.com>
2018-06-05 08:28:18 +0545

    created basic structure

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express'),
   app = express(),
   config = require('./config'),
   port = config.port,
+  hbs = require('express-handlebars');
 
-  const routes = require('./app/routes') (app);
+const routes = require('./app/routes') (app);
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  defaultDir: __dirname + '/app/views'
+}));
+
+app.set('view engine', 'hbs');
 
 app.listen(port, () => {
   console.log('Server Started Successfully...');
