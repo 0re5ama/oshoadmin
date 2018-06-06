ce5166358eec2d51abe22a067fce769ee10f553d
Bibek <shahbibek10@gmail.com>
2018-06-06 16:29:15 +0545

    changes in the programs and added some libraries

--- assets/js/programs.js
+++ assets/js/programs.js
@@ -13,6 +13,21 @@ var app = new Vue({
 			{ text: 'Sauraha', value: '2' },
 			{ text: 'Lumbini', value: '3' },
 			{ text: 'Nepalgunj', value: '4' }
+		],
+		acharyas: [
+			{ text: 'Osho Maitreya', value: '1' },
+			{ text: 'Osho Nirakar', value: '2' },
+			{ text: 'Osho Bashudev', value: '3' },
+			{ text: 'Osho Mahendra', value: '4' }
 		]
 	}
+});
+
+$(function () {
+	$('#fromDate').on('dp.change', function (e) {
+		$('#toDate').data('DateTimePicker').minDate(e.date);
+	});
+	$('#toDate').on('dp.change', function (e) {
+		$('#fromDate').data('DateTimePicker').maxDate(e.date);
+	});
 });
